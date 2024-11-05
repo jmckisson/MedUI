0c2f24711fc6734cd11f983e31aef7f6cb9bf89c
John McKisson <john.mckisson@gmail.com>
2024-11-05 08:30:22 -0500

    refactor chat code out of MedieviaTabbedChat folder

--- src/scripts/MedUI/HelpFile.lua
+++ src/scripts/MedUI/HelpFile.lua
@@ -3,7 +3,7 @@
 -- Created by Kronos using EMCOChat/MDK
 -- https://github.com/berood001/MedMudlet
 
--- Requires the EMCOChat and MDK Packages
+-- Requires the MDK Package
 
 -- MedieviaTabbedChat will build a window that can be unlocked, moved around, and resized as desired. 
 -- The window will contain tabs for different in game chat channels
