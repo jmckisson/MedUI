33a7d5eb53fa08018593b1c4f833c175d33a1e12
John McKisson <john.mckisson@gmail.com>
2024-12-29 14:48:44 -0500

    dont run trigger if generic_mapper doesnt exist

--- src/triggers/Medievia Mapper/MedieviaExitLineTrigger.lua
+++ src/triggers/Medievia Mapper/MedieviaExitLineTrigger.lua
@@ -1,2 +1,7 @@
+if not map then
+    return
+end
+
+-- Exit information is now in GMCP Room.Info, we should be using that instead
 map.prompt.exits = map.prompt.exits .. ", " .. string.trim(matches[2])
 setTriggerStayOpen("Medievia Multi-Line Exits Trigger",1)
