931610362ca79011cc32a3fb439d306dbee2d2b3
John McKisson <john.mckisson@gmail.com>
2025-01-19 15:55:58 -0500

    remove PromptConfig

--- src/triggers/MedUI/DefaultPromptConfig.lua
+++
@@ -1,7 +0,0 @@
-if not MedPrompt.setupComplete then
-  deleteLine()
-  moveCursor(0,getLineNumber()-1)
-  deleteLine()
-end
-
-MedPrompt.generatePromptRegex("none")
--- src/triggers/MedUI/PromptConfig.lua
+++
@@ -1,7 +0,0 @@
-if not MedPrompt.setupComplete then
-  deleteLine()
-  moveCursor(0,getLineNumber()-1)
-  deleteLine()
-end
-
-local output = MedPrompt.generatePromptRegex(multimatches[3][1])
